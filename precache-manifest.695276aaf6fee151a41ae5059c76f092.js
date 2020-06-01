ef865626d5aa9a7e2b014be1a47162aec6a24ce6
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-01 01:17:29 -0300

    Deploy GitHub Pages

---
+++ precache-manifest.695276aaf6fee151a41ae5059c76f092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e266afe4608a23081e475e3913d903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f2d28931ed6114b1bb4",
+    "url": "/static/js/2.a07cbd57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a07cbd57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955c5f4bf94d47ddd9ce",
+    "url": "/static/js/main.64fb080e.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "bb7885018e93f1b86018d4f2d331a705",
+    "url": "/static/media/banner.bb788501.jpg"
+  }
+]);
